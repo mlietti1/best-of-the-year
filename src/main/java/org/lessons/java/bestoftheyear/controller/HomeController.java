d226817fd5a002aaa46773b8b697f0317b17b780
mlietti1 <mcf.lietti@outlook.com>
2023-03-27 16:42:54 +0200

    step 2

--- src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
+++ src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
@@ -1,10 +1,16 @@
 package org.lessons.java.bestoftheyear.controller;
 
 
+import org.lessons.java.bestoftheyear.model.Movie;
+import org.lessons.java.bestoftheyear.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -15,4 +21,37 @@ public class HomeController {
         model.addAttribute("name", userName);
         return "home";
     }
+
+    private List<Song> getBestSongs(){
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song("2 Be Loved", 1));
+        songs.add(new Song("Jaded", 2));
+        songs.add(new Song("Good Days", 3));
+        return songs;
+    }
+
+    @GetMapping("/songs")
+    public String songs (Model model){
+        List<Song> list = getBestSongs();
+        String titles = list.stream().map(Song::getTitle).collect(Collectors.joining(", "));
+        model.addAttribute("songs", titles);
+        return "songs";
+    }
+
+    @GetMapping("/movies")
+    public String movies (Model model){
+        List<Movie> list = getBestMovies();
+        String titles = list.stream().map(Movie::getTitle).collect(Collectors.joining(", "));
+        model.addAttribute("movies", titles);
+        return "movies";
+    }
+
+    private List<Movie> getBestMovies(){
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("Do Revenge", 1));
+        movies.add(new Movie("Everything Everywhere All at Once", 2));
+        movies.add(new Movie("The Dirt", 3));
+        return movies;
+    }
+
 }
---
+++ src/main/java/org/lessons/java/bestoftheyear/model/Movie.java
@@ -0,0 +1,27 @@
+package org.lessons.java.bestoftheyear.model;
+
+public class Movie {
+    private String title;
+    private int id;
+
+    public Movie(String title, int id) {
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
